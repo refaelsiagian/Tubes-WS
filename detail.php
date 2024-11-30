0a546d9be95758401ec9bd176082a413f1849baf
unknown <mayadisilalahi@gmail.com>
2024-11-30 13:47:52 +0700

    menampilkan gmaps

--- detail.php
+++ detail.php
@@ -1,33 +1,39 @@
 <?php
-    include 'vendor/functions.php';
+include 'vendor/functions.php';
 
-    if (isset($_GET['destination'])) {
-        $destination = urldecode($_GET['destination']);
+if (isset($_GET['destination'])) {
+    $destination = urldecode($_GET['destination']);
+}
+
+$query = '
+
+    SELECT DISTINCT ?name ?province ?description ?thumbnail ?category ?location ?built ?discovered 
+    WHERE {
+        ?subject rdfs:name ?name.
+        ?subject places:province ?province.
+        ?subject places:description ?description.
+        ?subject places:thumbnail ?thumbnail.
+        ?subject places:category ?category.
+        ?subject places:location ?location.
+        ?subject places:built ?built.
+        ?subject places:discovered ?discovered.
+
+        FILTER(?name = "'.$destination.'")
     }
+';
 
-    $query = '
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX places: <https://example.org/schema/places>
-
-        SELECT ?name ?province ?description ?thumbnail ?category ?location ?built ?discovered
-        WHERE {
-            ?subject rdfs:name ?name.
-            ?subject places:province ?province.
-            ?subject places:description ?description.
-            ?subject places:thumbnail ?thumbnail.
-            ?subject places:category ?category.
-            ?subject places:location ?location.
-            ?subject places:built ?built.
-            ?subject places:discovered ?discovered.
-            FILTER(?name = "'.$destination.'")
-        }
-    ';
+$mapQuery = '
+    SELECT DISTINCT * WHERE {
+        dbr:' . str_replace(' ', '_', $destination) . ' geo:lat  ?lat;
+                                                       geo:long ?long .
+    }
+';
 
 ?>
 
 <?php 
-    $title = $destination;
-    include 'include/header.php';
+$title = $destination;
+include 'include/header.php';
 ?>
 
 <div class="header-top">
@@ -42,48 +48,62 @@
 
 <div class="container-content">
     <div class="content">
-        <?php try { $result = $sparql_jena->query($query);?>
-            <?php if(count($result) > 0) : ?>
+        <?php 
+        $result = $sparql_jena->query($query);
 
-                <?php foreach($result as $row) : ?>
-                    <div class="card-content-img">
-                        <img alt="<?= $destination; ?>" height="400" src="<?= $row->thumbnail; ?>" width="300"/>
+        if ($result && count($result) > 0): // Periksa apakah ada hasil dari query ?>
+            <?php foreach ($result as $row): ?>
+                <?php 
+                // Mendapatkan koordinat lokasi
+                $result2 = $sparqlDbPedia->query($mapQuery)->current();
+                ?>
+                <div class="card-content-img">
+                    <img alt="<?= $destination; ?>" height="400" src="<?= $row->thumbnail; ?>" width="300"/>
+                </div>
+                <div class="card-content">
+                    <div class="text-content">
+                        <h2><?= $row->name; ?></h2>
+                        <p><?= $row->description; ?></p>
                     </div>
-                    <div class="card-content">
+                    <table class="info-table">
+                        <tr>
+                            <th>Ditemukan</th>
+                            <td><?= $row->discovered; ?></td>
+                        </tr>
+                        <tr>
+                            <th>Didirikan</th>
+                            <td><?= $row->built; ?></td>
+                        </tr>
+                        <tr>
+                            <th>Lokasi</th>
+                            <td><?= $row->location; ?></td>
+                        </tr>
+                        <tr>
+                            <th>Provinsi</th>
+                            <td><?= $row->province; ?></td>
+                        </tr>
+                    </table>
+                    <div class="card-content mt-10">
                         <div class="text-content">
-                            <!-- <h3>Tempat Ibadah</h3> -->
-                            <h2><?= $row->name; ?></h2>
-                            <p><?= $row->description; ?></p>
+                            <!-- Menampilkan lokasi pada peta -->
+                            <div class="mt-4">
+                                <?php if ($result2): ?>
+                                    <iframe width="100%" height="376.875px" style="border:0;" allowfullscreen="" 
+                                            loading="lazy" referrerpolicy="no-referrer-when-downgrade" 
+                                            src="https://maps.google.com/maps?q=<?= $result2->lat ?>,<?= $result2->long ?>&hl=en&z=14&amp;output=embed">
+                                    </iframe>
+                                
+                                <?php endif; ?>
+                            </div>
                         </div>
-                        <table class="info-table">
-                            <tr>
-                                <th>Ditemukan</th>
-                                <td><?= $row->discovered; ?></td>
-                            </tr>
-                            <tr>
-                                <th>Didirikan</th>
-                                <td><?= $row->built; ?></td>
-                            </tr>
-                            <tr>
-                                <th>Lokasi</th>
-                                <td><?= $row->location; ?></td>
-                            </tr>
-                            <tr>
-                                <th>Provinsi</th>
-                                <td><?= $row->province; ?></td>
-                            </tr>
-                        </table>
                     </div>
-                <?php endforeach; ?>
 
-            <?php else : ?>
-                <h2>No data found</h2>
-            <?php endif; ?>
+                </div>
+            <?php endforeach; ?>
 
-        <?php } catch (Exception $e) { ?>
-            <h2>Terjadi kesalahan: <?= $e->getMessage() ?></h2>
-        <?php } ?>
-
+        <?php else: ?>
+            <h2>No data found</h2>
+        <?php endif; ?>
     </div>
 </div>
 
