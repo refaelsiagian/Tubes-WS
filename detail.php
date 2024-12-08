702eb377b76d9caf1c329ff0a66c1c09f57841db
unknown <mayadisilalahi@gmail.com>
2024-12-08 22:14:16 +0700

    perbaiki fitur gmaps

--- detail.php
+++ detail.php
@@ -5,9 +5,9 @@ if (isset($_GET['destination'])) {
     $destination = urldecode($_GET['destination']);
 }
 
+// Query SPARQL untuk mengambil data
 $query = '
-
-    SELECT DISTINCT ?name ?province ?description ?thumbnail ?category ?location ?built ?discovered 
+    SELECT DISTINCT ?name ?province ?description ?thumbnail ?category ?location ?built ?discovered ?subject
     WHERE {
         ?subject rdfs:name ?name.
         ?subject places:province ?province.
@@ -17,15 +17,7 @@ $query = '
         ?subject places:location ?location.
         ?subject places:built ?built.
         ?subject places:discovered ?discovered.
-
         FILTER(?name = "'.$destination.'")
-    }
-';
-
-$mapQuery = '
-    SELECT DISTINCT * WHERE {
-        dbr:' . str_replace(' ', '_', $destination) . ' geo:lat  ?lat;
-                                                       geo:long ?long .
     }
 ';
 
@@ -38,24 +30,44 @@ include 'include/header.php';
 
 <div class="header-top">
     <div class="header-image"></div>
-       <div class="text-header">
-          <div class="center">
-             <h1><?= $title; ?></h1>
-          </div>
-        </div>  
-    </div>
+    <div class="text-header">
+        <div class="center">
+            <h1><?= $title; ?></h1>
+        </div>
+    </div>  
 </div>
 
 <div class="container-content">
     <div class="content">
         <?php 
+        // Eksekusi query SPARQL
         $result = $sparql_jena->query($query);
 
         if ($result && count($result) > 0): // Periksa apakah ada hasil dari query ?>
             <?php foreach ($result as $row): ?>
                 <?php 
-                // Mendapatkan koordinat lokasi
-                $result2 = $sparqlDbPedia->query($mapQuery)->current();
+                // Periksa apakah 'about' ada dan valid
+                $aboutValue = '';
+                if (isset($row->subject) && filter_var($row->subject, FILTER_VALIDATE_URL)) {
+                    $aboutValue = basename(parse_url($row->subject, PHP_URL_PATH)); // Mendapatkan nama resource
+                }
+
+                // Query SPARQL untuk mendapatkan koordinat
+                if ($aboutValue) {
+                    $mapQuery = '
+                    National_Monument_(Indonesia)
+                    ';
+                    $result2 = $sparqlDbPedia->query($mapQuery)->current();
+                }
+
+                // Menyiapkan data koordinat dengan default jika tidak ada
+                if ($result2 && isset($result2->lat) && isset($result2->long)) {
+                    $lat = $result2->lat;
+                    $long = $result2->long;
+                } else {
+                    $lat = 0;  // Nilai default
+                    $long = 0; // Nilai default
+                }
                 ?>
                 <div class="card-content-img">
                     <img alt="<?= $destination; ?>" height="400" src="<?= $row->thumbnail; ?>" width="300"/>
@@ -87,12 +99,11 @@ include 'include/header.php';
                         <div class="text-content">
                             <!-- Menampilkan lokasi pada peta -->
                             <div class="mt-4">
-                                <?php if ($result2): ?>
+                                <?php if ($lat && $long): ?>
                                     <iframe width="100%" height="376.875px" style="border:0;" allowfullscreen="" 
                                             loading="lazy" referrerpolicy="no-referrer-when-downgrade" 
-                                            src="https://maps.google.com/maps?q=<?= $result2->lat ?>,<?= $result2->long ?>&hl=en&z=14&amp;output=embed">
+                                            src="https://maps.google.com/maps?q=<?= $lat ?>,<?= $long ?>&hl=en&z=14&amp;output=embed">
                                     </iframe>
-                                
                                 <?php endif; ?>
                             </div>
                         </div>
